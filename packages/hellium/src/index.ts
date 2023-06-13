e0919726feb60142db0ef5751a792f62e9d6a0b9
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-13 14:11:40 -0400

    export VNode type

--- packages/hellium/src/index.ts
+++ packages/hellium/src/index.ts
@@ -1,3 +1,3 @@
 export { h } from "./lib/hyperscript";
 export { default as render } from "./lib/render";
-export type { Tag, Props, VNodeChildren } from "./lib/types/vnode";
+export type { Tag, Props, VNodeChildren, VNode } from "./lib/types/vnode";
