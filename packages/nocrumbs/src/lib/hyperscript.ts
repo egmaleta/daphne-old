b26bbbfcdff0eb398c34db6a9f9d7aaf5d823b54
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-14 12:48:48 -0400

    refactor h and remove slotted children extraction

--- packages/nocrumbs/src/lib/hyperscript.ts
+++ packages/nocrumbs/src/lib/hyperscript.ts
@@ -7,44 +7,22 @@ export function h<T extends Tag | Component, P extends Record<string, any>>(
   type: T,
   props: P & { children?: VNodeChild | VNodeChildren }
 ): VNodeChild | VNodeChildren {
-  // eslint-disable-next-line prefer-const
-  let { children, ...rest } = props;
-
   // make sure children is a list
-  if (!children) {
-    children = [];
-  } else if (!Array.isArray(children)) {
-    children = [children];
+  if (!props.children) {
+    props.children = [];
+  } else if (!Array.isArray(props.children)) {
+    props.children = [props.children];
+  } else {
+    // flatten children
+    props.children = [...flatten(props.children)];
   }
 
-  // flatten children
-  children = [...flatten(children)];
-
   if (typeof type !== "function") {
+    const { children, ...rest } = props;
     return { tag: type, props: rest, children };
   }
 
   // type is a function => jsx expr is a component
   const component = type as Component;
-
-  // look for slotted children by filtering children
-  // if a child has a slot prop, filter it out
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const slottedChildren: Record<string, any> = {};
-  children = children.filter((child) => {
-    if (typeof child === "object" && child !== null) {
-      const { slot, ...childRest } = child.props;
-      child.props = childRest;
-
-      if (slot) {
-        slottedChildren[slot] = child;
-        return false;
-      }
-    }
-    return true;
-  });
-
-  const componentProps = Object.assign(rest, { children, ...slottedChildren });
-
-  return attachLifecycleHookTriggers(component, componentProps);
+  return attachLifecycleHookTriggers(component, props);
 }
