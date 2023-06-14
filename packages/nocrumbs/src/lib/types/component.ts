5b47a3cad07af0c284f28992c65980f75ef25a72
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-13 22:24:35 -0400

    fix Component type and import as type

--- packages/nocrumbs/src/lib/types/component.ts
+++ packages/nocrumbs/src/lib/types/component.ts
@@ -1,6 +1,6 @@
-import { VNodeChildren } from "./vnode";
+import type { VNodeChild, VNodeChildren } from "./vnode";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Component<P extends Record<string, any> = object> = (
   props: P & { children?: VNodeChildren }
-) => VNodeChildren | VNodeChildren;
+) => VNodeChildren | VNodeChild;
