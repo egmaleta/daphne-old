d195a1d36bd9ecedf6a69096808b4d401fc5645c
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-14 09:57:58 -0400

    export signals from nocrumbs package

---
+++ packages/nocrumbs/src/signals.ts
@@ -0,0 +1,1 @@
+export * from "@nocrumbs/signals";
--- packages/nocrumbs/vite.config.ts
+++ packages/nocrumbs/vite.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     lib: {
-      entry: ["src/index.ts", "src/jsx-runtime.ts", "src/jsx-dev-runtime.ts"],
+      entry: [
+        "src/index.ts",
+        "src/jsx-runtime.ts",
+        "src/jsx-dev-runtime.ts",
+        "src/signals.ts",
+      ],
       formats: ["es", "cjs"],
       fileName: (format, name) =>
         `${name}.${format === "cjs" ? "common" : format}.js`,
