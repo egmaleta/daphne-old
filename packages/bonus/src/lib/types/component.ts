61e410d85e8f6433639d54fe30f05aa2caf0bbe5
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-16 09:13:11 -0400

    remove eslint-disable comment

--- packages/bonus/src/lib/types/component.ts
+++ packages/bonus/src/lib/types/component.ts
@@ -1,6 +1,5 @@
 import type { VNodeChild, VNodeChildren } from "./vnode";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Component<
   P extends Record<string, any> = object,
   C extends VNodeChildren | VNodeChild = VNodeChildren | VNodeChild
