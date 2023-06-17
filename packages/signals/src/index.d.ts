6d397c7aba08b167976d849f3bc3e0168385a0c5
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-06-17 11:32:09 -0400

    fixed type declarations

---
+++ packages/signals/src/index.d.ts
@@ -0,0 +1,5 @@
+import type { ReadonlySignal, Signal } from "./lib/types";
+export type { Signal, ReadonlySignal };
+export declare const signal: <T extends unknown>(initialValue: T) => Signal<T>;
+export declare const computed: <T extends unknown>(computation: () => T) => ReadonlySignal<T>;
+export declare const effect: (computation: () => void) => void;
